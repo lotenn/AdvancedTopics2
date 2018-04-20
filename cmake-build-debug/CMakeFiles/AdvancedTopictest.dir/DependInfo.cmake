b75b83c64732661431425c773dc95e727bcc750e
lotenn <38076190+lotenn@users.noreply.github.com>
2018-04-20 11:59:25 +0300

    sadsda

--- cmake-build-debug/CMakeFiles/AdvancedTopictest.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/AdvancedTopictest.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/loten/CLionProjects/AdvancedTopictest/main.cpp" "C:/Users/loten/CLionProjects/AdvancedTopictest/cmake-build-debug/CMakeFiles/AdvancedTopictest.dir/main.cpp.obj"
+  "C:/Users/loten/CLionProjects/AdvancedTopics2/main.cpp" "C:/Users/loten/CLionProjects/AdvancedTopics2/cmake-build-debug/CMakeFiles/AdvancedTopictest.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
