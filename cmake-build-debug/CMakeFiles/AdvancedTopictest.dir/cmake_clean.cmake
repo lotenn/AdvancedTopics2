9bf3d820c341eff9175d547ae5732f57c8b71b92
lotenn <38076190+lotenn@users.noreply.github.com>
2018-04-20 11:56:16 +0300

    init

---
+++ cmake-build-debug/CMakeFiles/AdvancedTopictest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AdvancedTopictest.dir/main.cpp.obj"
+  "AdvancedTopictest.pdb"
+  "AdvancedTopictest.exe"
+  "AdvancedTopictest.exe.manifest"
+  "libAdvancedTopictest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AdvancedTopictest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
