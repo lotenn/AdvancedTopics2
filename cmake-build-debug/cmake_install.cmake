d9d1fdaa79a93fbe0effb18f173bbc88c2e3abd7
Saar <saar157@gmail.com>
2018-05-04 10:50:31 +0300

    update

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/loten/CLionProjects/AdvancedTopics2
+# Install script for directory: C:/Users/Saar/CLionProjects/AdvancedTopics2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/loten/CLionProjects/AdvancedTopics2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Saar/CLionProjects/AdvancedTopics2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
